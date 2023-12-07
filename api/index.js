b7119f5eea4f64523544767bb9ecc9a172991b34
Anirudh Acharya <anirudhacharya134@gmail.com>
2023-12-08 03:32:24 +0530

    create sign up ap route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express' ;
 import mongoose from 'mongoose' ;
 import dotenv from 'dotenv' ;
 import userRoutes from './routes/user.route.js' // Add .js in the end
+import authRoutes from './routes/auth.route.js' // Add .js in the end
 dotenv.config();
 
 
@@ -13,8 +14,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+app.use(express.json()); // used for sending json data through http requests
+
 app.listen(3000, ()=>{
     console.log('server listening to port number 3000.')
 })
 
+//Adding api routes khudse banaya hai yeh /api/user aur /api/auth
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
