6a15eeeb6283c61b10e32e0e0e94a20e6e082c09
Anirudh Acharya <anirudhacharya134@gmail.com>
2023-12-07 07:10:05 +0530

    connection to db

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose' ;
 import dotenv from 'dotenv' ;
 dotenv.config();
 
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connected to Database');
 }).catch((err)=>{
